45c2ae94c74aa8068d06407e1869da65a532f2de
judiefriend <105896508+judiefriend@users.noreply.github.com>
2022-08-28 14:31:18 -0600

    update content

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World! This is Phoenix trying out some cool stuff with AWS'.
-The good news is that my build worked! No i have added a review step so i am adding this text to make a new commit.));
+The good news is that my build worked! No i have added a review step so i am adding this text to make a new commit.
+                                   It worked but the bad news is that the health of my app server is severe so i could nout view my changes on the command line even though my build was successful));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
