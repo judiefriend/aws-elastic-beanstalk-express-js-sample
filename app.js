e13828ec373a32a79f58928df7ddf7894edb3182
judiefriend <105896508+judiefriend@users.noreply.github.com>
2022-08-28 15:01:53 -0600

    Update content with experience

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Folks! This is cohort7 Grp4 moving tins!'));
+app.get('/', (req, res) => res.send('Hello Folks! This is cohort7 Grp4 moving tins!'
+                                   I wasn't part of the last session but i enjoyed watching the videos. I was able to successfully build, run my codepipeline and delete all the resources. 
+                                    I did notice that health monitoring is set up when we setup out Amazon Elastic Beanstalk. For some strange reason after I ran my first build, I made some code changes then ran another successful build.
+                                    This time around my health monitoring had the status as severe and i was unable to view my application in the URL
+                                   I hope someone runs this build from here so that you can see this my plenty talk!));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
