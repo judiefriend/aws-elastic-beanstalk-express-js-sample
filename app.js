f212b33d4ed381317c45a536fe4d718ab89df02a
Judith Adewoye <judith.adewoye@gmail.com>
2022-08-28 14:20:34 -0600

    Correct sentence

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World! This is Phoenix trying out some cool stuff with AWS'.
-The good news is that mu build worked! No i have added a review step so i am adding this text to make a new commit.));
+The good news is that my build worked! No i have added a review step so i am adding this text to make a new commit.));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
