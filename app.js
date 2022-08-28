9386e7cf6b7a168472b62a6ec7dbe081e6d9062c
Judith Adewoye <judith.adewoye@gmail.com>
2022-08-28 14:19:21 -0600

    add new sentence

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Phoenix trying out some cool stuff with AWS'));
+app.get('/', (req, res) => res.send('Hello World! This is Phoenix trying out some cool stuff with AWS'.
+The good news is that mu build worked! No i have added a review step so i am adding this text to make a new commit.));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
